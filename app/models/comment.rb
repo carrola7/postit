0b499ee861752d8f4f540c9b6157e256ab008c2a
Adrian Carroll <adrian.carroll7@gmail.com>
2018-12-16 21:29:52 +0000

    Added post all post view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,3 +2,4 @@ class Comment < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
   belongs_to :post, foreign_key: :post_id
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181212071032) do
+ActiveRecord::Schema.define(version: 20181214060957) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
-    t.text    "body"
-    t.integer "user_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer "category_id"
     t.integer "post_id"
   end
 
   create_table "posts", force: true do |t|
-    t.string  "url"
-    t.string  "title"
-    t.text    "description"
-    t.integer "user_id"
+    t.string   "url"
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string "username"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
