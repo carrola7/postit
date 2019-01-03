336d4b10c0038f95c9bcb02b4a8509d94ed8e464
Adrian Carroll <adrian.carroll7@gmail.com>
2019-01-03 20:34:39 +0000

    fixed sluggable error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :creator, foreign_key: :user_id, class_name: 'User'
   belongs_to :post, foreign_key: :post_id
 
   validates :body, presence: true
 
-  include Voteable
 
 end
 
